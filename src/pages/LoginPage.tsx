14e9fa15b70e465cb13b876c150ac8c0c205fcc0
Mikael Larsson <s.mikael.larsson@gmail.com>
2025-02-18 13:34:57 +0100

    skapa loginformulär

--- src/pages/LoginPage.tsx
+++ src/pages/LoginPage.tsx
@@ -3,14 +3,37 @@ import "./css/LoginPage.css";
 
 const LoginPage = () => {
 
-const [email, setEmail] = useState("");
-const [password, setPassword] = useState("");
-const [error, setError] = useState("");
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+    const [error, setError] = useState("");
+
+    const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+        e.preventDefault();
+        setError("");
+    };
+
 
     return (
-        <>
-        <h1>Logga in</h1>
-        </>
+        <div className="loginContainer">
+            <div className="loginBox">
+
+                <h1>Logga in</h1>
+                <form onSubmit={handleSubmit}>
+                    <div className="errorMessage">{error && <p>{error}</p>}</div>
+
+                    <label htmlFor="email">E-postadress:</label>
+                    <br/>
+                    <input id="email" type="email" required value={email} onChange={(e) => setEmail(e.target.value)} />
+                    <br />
+                    <label htmlFor="password">Lösenord:</label>
+                    <br />
+                    <input id="password" type="password" required value={password} onChange={(e) => setPassword(e.target.value)}/>
+
+                    <br />
+                    <button type="submit">Logga in</button>
+                </form>
+            </div>
+        </div>
     )
 }
 
