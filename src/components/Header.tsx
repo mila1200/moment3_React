f1958f45c33b184c896742825514dd4ea178ef09
Mikael Larsson <s.mikael.larsson@gmail.com>
2025-02-19 10:23:33 +0100

    logga ut användare

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,14 +1,22 @@
 import { NavLink } from "react-router-dom"
+import { useAuth } from "../context/AuthContext"
 
 
 const Header = () => {
+
+  const {user, logout} = useAuth();
+
   return (
     <>
     <header>
         <ul>
             <li><NavLink to="/">Start</NavLink></li>
             <li><NavLink to="/login">Logga in</NavLink></li>
-            <li><NavLink to="/manageproducts">Hantera produkter</NavLink></li>
+            <li>
+              {
+                !user ?  <NavLink to="/manageproducts">Hantera produkter</NavLink> : <button onClick={logout}>Logga ut</button>
+              }
+             </li>
         </ul>
     </header>
     </>
